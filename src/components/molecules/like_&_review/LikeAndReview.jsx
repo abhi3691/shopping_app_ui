065d82916159f99c79ae05f822e2ef7af85124ec
abhinand v k <abhinandazokdio@gmail.com>
2022-09-17 17:42:55 +0530

    updated

--- src/components/molecules/like_&_review/LikeAndReview.jsx
+++ src/components/molecules/like_&_review/LikeAndReview.jsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableHighlight} from 'react-native';
 import React from 'react';
 import Icons from 'react-native-vector-icons/Ionicons';
 import customColor from '../../../theme/Color';
@@ -7,15 +7,26 @@ import FontRatio from '../../../global_functions/font_ratio/FontRatio';
 import {Rating} from 'react-native-ratings';
 
 const LikeAndReview = ({}) => {
+  //state
   const [like, setLike] = React.useState(false);
+
+  //change like function
+  const changeLike = () => {
+    setLike(!like);
+  };
+
+  //like and review UI
   return (
     <View style={styles.container}>
-      <Icons
-        name={like ? 'heart' : 'heart-outline'}
-        color={like ? customColor.red : customColor.gray}
-        size={FontRatio(20)}
-      />
-
+      <TouchableHighlight
+        underlayColor={'transparent'}
+        onPress={() => changeLike()}>
+        <Icons
+          name={like ? 'heart' : 'heart-outline'}
+          color={like ? customColor.red : customColor.gray}
+          size={FontRatio(20)}
+        />
+      </TouchableHighlight>
       <View style={styles.ratingContainer}>
         <Rating type="star" ratingCount={5} imageSize={18} />
         <Text style={styles.ratingCount}>50 Ratings</Text>
