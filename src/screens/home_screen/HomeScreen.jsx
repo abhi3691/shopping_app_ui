23fb7e8113ce1fa44bec4c55240dc54c2a358f5b
abhinand v k <abhinandazokdio@gmail.com>
2022-09-17 13:47:26 +0530

    updated

--- src/screens/home_screen/HomeScreen.jsx
+++ src/screens/home_screen/HomeScreen.jsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import styles from './HomeScreenStyle'
 import HomeHeader from '../../components/organization/home_header/HomeHeader'
 import RequestList from '../../components/template/reqest_list/RequestList'
+import customColor from '../../theme/Color'
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={customColor.blue}
+      />
       {/* //header */}
       <HomeHeader name={"john"} />
 
