21f38688a3cc28fe12b5629a060de411e7e0ccca
abhinand v k <abhinandazokdio@gmail.com>
2022-09-17 16:40:41 +0530

    updated

---
+++ src/components/molecules/like_&_review/LikeAndReview.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Icons from 'react-native-vector-icons/Ionicons';
+
+const LikeAndReview = () => {
+  return (
+    <View>
+      <Icons name="" />
+    </View>
+  );
+};
+
+export default LikeAndReview;
